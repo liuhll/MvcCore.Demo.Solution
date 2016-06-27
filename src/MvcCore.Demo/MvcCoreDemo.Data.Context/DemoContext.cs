de96c7dd75f69019c3d9612f9e56ec681d17d597
liuhongliang <1029765111@qq.com>
2016-06-27 21:34:08 +0800

    构建数据上下文对象

---
+++ src/MvcCore.Demo/MvcCoreDemo.Data.Context/DemoContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Threading.Tasks;
+using MvcCoreDemo.Data.Context.Config;
+using MvcCoreDemo.Data.Context.Mapping;
+using MvcCoreDemo.Domain.Entities.Model;
+
+namespace MvcCoreDemo.Data.Context
+{
+    public class DemoContext : BaseDbContext
+    {
+        public DemoContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Configurations.Add(new GenreMap());
+            modelBuilder.Configurations.Add(new ArtistMap());
+            modelBuilder.Configurations.Add(new AlbumMap());
+            modelBuilder.Configurations.Add(new CartMap());
+            modelBuilder.Configurations.Add(new OrderMap());
+            modelBuilder.Configurations.Add(new OrderDetailMap());
+        }
+
+        #region DbSet
+
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+        #endregion
+    }
+}
