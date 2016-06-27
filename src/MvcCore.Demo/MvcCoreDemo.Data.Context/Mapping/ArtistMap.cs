351ffa7dd9163455b31e6da2c7ec738805449b98
liuhongliang <1029765111@qq.com>
2016-06-27 20:26:58 +0800

    添加实体之间映射关系对象

---
+++ src/MvcCore.Demo/MvcCoreDemo.Data.Context/Mapping/ArtistMap.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Threading.Tasks;
+using MvcCoreDemo.Domain.Entities.Model;
+
+namespace MvcCoreDemo.Data.Context.Mapping
+{
+    public class ArtistMap : EntityTypeConfiguration<Artist>
+    {
+        public ArtistMap()
+        {
+            // Primary Key
+            HasKey(t => t.ID);
+
+            // Properties
+            Property(t => t.Name)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            Ignore(t => t.ValidationResult);
+        }
+    }
+}
---
+++ src/MvcCore.Demo/MvcCoreDemo.Data.Context/Mapping/CartMap.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Threading.Tasks;
+using MvcCoreDemo.Domain.Entities.Model;
+
+namespace MvcCoreDemo.Data.Context.Mapping
+{
+    public class CartMap : EntityTypeConfiguration<Cart>
+    {
+        public CartMap()
+        {
+            // Primary Key
+            HasKey(t => t.ID);
+
+            // Properties
+            //Property(t => t.CartId)
+            //    .IsRequired();
+
+            Property(t => t.AlbumId)
+                .IsRequired();
+
+            Property(t => t.Count)
+                .IsRequired();
+
+            Property(t => t.DateCreated)
+                .IsRequired();
+
+            Ignore(t => t.ValidationResult);
+        }
+    }
+}
